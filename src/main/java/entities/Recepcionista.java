ba60de95b7f8f741fafcdae9ba31241321a8595e
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2024-05-07 23:15:06 -0300

    Update Recepcionista.java

--- src/main/java/entities/Recepcionista.java
+++ src/main/java/entities/Recepcionista.java
@@ -1,4 +1,27 @@
-package entities;
+package hotel_system;
 
-public class Recepcionista {
+public class Recepcionista extends Thread{
+	
+	public Recepcionista(String nome){
+		setName(nome);
+	}
+	
+	@Override
+	public void run() {
+		while (!Hotel.getFilaEspera().isEmpty()) {
+			Quarto quarto = Hotel.quartoDisponivel();
+			if (quarto != null) {
+				if (quarto.reservar()) {
+					Hospede hospede = Hotel.getHospede();
+					if (hospede != null) {
+						System.out.println("Hospedando cliente: " + hospede.getName() + " -  quarto: " + quarto.getNumero());
+						hospede.hospedar(quarto);
+					}
+					
+				}
+			}
+		}
+		
+		System.out.println("Acabou turno recepcionista");
+	}
 }
