a8273635f7e3a4b3a6fcaa51256da4f45953b2db
Arthur Neves <80648797+ArtutsN@users.noreply.github.com>
2024-05-07 23:29:53 -0300

    Update Camareira.java

--- src/main/java/entities/Camareira.java
+++ src/main/java/entities/Camareira.java
@@ -1,4 +1,28 @@
 package entities;
 
-public class Camareira {
+import java.util.List;
+
+public class Camareira extends Thread{
+	
+	public Camareira(String nome) {
+		this.setName(nome);
+	}
+
+	@Override
+	public void run() {
+		boolean loop = true;
+		int contador = 0;
+		while (loop) {
+			boolean quartoLimpo = Hotel.quartoLimpo();
+			if (quartoLimpo) {
+				Hotel.getQuartos().forEach(Quarto::limpar);
+				contador++;
+			}
+			if(contador == 50){
+				loop = false;
+			}
+		}
+	}
+
+
 }
